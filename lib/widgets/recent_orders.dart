861f76f31aa46de0289fd9557d70f3a30facbcab
ibtehajraza <ibtahajraza@gmail.com>
2020-04-24 23:10:20 +0300

    Finished Recent orders Scroller.

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -4,7 +4,7 @@ import '../data/data.dart';
 import '../models/order.dart';
 
 class RecentOrders extends StatelessWidget {
-  Widget _RecentOrders(BuildContext context, Order order) {
+  Widget _recentOrders(BuildContext context, Order order) {
     return Container(
       margin: EdgeInsets.all(10.0),
       width: 320.0,
@@ -17,29 +17,79 @@ class RecentOrders extends StatelessWidget {
         ),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image(
-              height: 100.0,
-              width: 100.0,
-              image: AssetImage(order.food.imageUrl),
-              fit: BoxFit.cover,
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 100.0,
+                    width: 100.0,
+                    image: AssetImage(order.food.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          order.date,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              
-              Text(
-                order.food.name,
-                style: TextStyle(),
-              ),
-              Text(order.restaurant.name),
-              Text(order.date),
-            ],
-          )
+          Container(
+            margin: EdgeInsets.only(
+              right: 20.0,
+            ),
+            width: 48.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                color: Colors.white,
+                onPressed: () {}),
+          ),
         ],
       ),
     );
@@ -65,11 +115,13 @@ class RecentOrders extends StatelessWidget {
           height: 120.0,
           //color: Colors.blue,
           child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.only(left:10.0),
               scrollDirection: Axis.horizontal,
               itemCount: currentUser.orders.length,
               itemBuilder: (BuildContext context, int index) {
                 Order order = currentUser.orders[index];
-                return _RecentOrders(context, order);
+                return _recentOrders(context, order);
               }),
         ),
       ],
